51a496c3558189966889b84116605952f1b21f3a
Arjun <arjuntheeng93@gmail.com>
2026-06-05 11:53:39 +0545

    feat migrate all endpoint to PostgreSq database

--- main.py
+++ main.py
@@ -1,47 +1,75 @@
 import uvicorn# this is a server which boardcast code to internet so web browser can talk to it
 from fastapi import FastAPI # handel repetivity parts automatically
 from pydantic import BaseModel # it follow the rule we set
+from database import engine, Base
+import model
+
+Base.metadata.create_all(bind=engine) # it create database and table for us
+from database import SessionLocal # it create session for us to interact with database  
+from sqlalchemy.orm import Session # it help us to interact with database
+from fastapi import Depends # it help us to manage dependencies
+
+def get_db():
+    db = SessionLocal() # it create a session
+    try:
+        yield db # it return the session to the caller
+    finally:
+        db.close() # it close the session after use  
+
 app = FastAPI()# this build our web application 
 
-class MenuItem(BaseModel):# this is a blueprint we are creating requirement type
+class MenuItemCreate(BaseModel):# this is a blueprint we are creating requirement type
+    name: str
+    price: float
+class MenuItemResponse(BaseModel):
     id: int
     name: str
     price: float
+    class Config:
+        from_attributes = True # it will allow us to use the attribute name instead of the field name in the response
 
-menu = [
-    {"id": 1, "name": "Pizza", "price": 10.99},
-    {"id": 2, "name":"Burger", "price": 8.99},
-    {"id": 3, "name": "Pasta", "price": 12.99},
-    {"id": 4, "name": "Salad", "price": 7.99},
-    {"id": 5, "name": "Soup", "price": 5.99},
-    {"id": 6, "name": "Sandwich", "price": 6.99},
-    {"id": 7, "name": "Taco", "price": 9.99},
-]
+
 @app.get("/")# this is a home page of api
 def home():
     return {"message":"Kitchen Management System"}
 @app.get("/menu") # this will retun all the menu we have written above
-def get_menu():
-    return menu
+def get_menu(db: Session = Depends(get_db)):
+    items = db.query(model.MenuItem).all()
+    return items# it will query all the items from database
+    
 
 @app.post("/menu")
-def add_menu_item(item: MenuItem):#if some one sends post request to the address it will tigger menu function
-    menu.append(item.dict()) # it take new items
-    return {"message": "Menu item added successfully", "item": item}
-
+def add_menu_item(item: MenuItemCreate, db: Session = Depends(get_db)):
+    #if some one sends post request to the address it will tigger menu function
+    db_item = model.MenuItem( name=item.name, price=item.price) # it will create a new item in database
+    db.add(db_item) # it will add the item to database
+    db.commit() # it will commit the changes to database
+    db.refresh(db_item) # it will refresh the item to get the id from database
+    return db_item # it will return the item we just added to database
 @app.delete("/menu/{item_id}") # it tell the server if someone want to delete items
-def delete_menu_item(item_id: int):
-    for item in menu: # it will look every item on the menu
-        if item["id"] == item_id:# it check the id we want to delete
-            menu.remove(item)# if match it remove
-            return {"message": "Menu item deleted successfully"}
-    return {"message": "Menu item not found"} # if nothing is found it will show this
+def delete_menu_item(item_id: int, db: Session = Depends(get_db)):
+    db_item = db.query(model.MenuItem).filter(model.MenuItem.id == item_id).first() # it will query the item with the given id
+    if db_item:
+        db.delete(db_item) # it will delete the item from database
+        db.commit() # it will commit the changes to database
+        return {"message": "Menu item deleted successfully"}
+    
+       
+    
 
 @app.put("/menu/{item_id}")# updating the existing data of item  id
-def update_item(item_id: int, item : MenuItem):
-    for item in menu:
-        if item["id"] == item_id:
-            item["name"] = update_item.name # it rewrite the old  name one to new one
-            item["price"] = update_item.price # it rewrite the old price to new one
-            return {"message": "Menu item updated successfully", "item": item} #it return back and ipdate new item
-    return{"message": "Menu item not found"}#if didnt found matching
+def update_menu_item(item_id: int, item: MenuItemCreate, db: Session = Depends(get_db)):
+    db_item = db.query(model.MenuItem).filter(model.MenuItem.id == item_id).first()
+    if not db_item:
+        return {"message": "mennu item not found"}
+    db_item.name = item.name
+    db_item.price = item.price
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+      
+
+@app.get("/search") # it will search the food by name and return the result
+def search_food(name: str, db: Session= Depends(get_db)): # it take the name of food as input
+    results = db.query(model.MenuItem).filter(model.MenuItem.name.ilike(f"%{name}%")).all() 
+    return {"results": results}  # it return the result of search
